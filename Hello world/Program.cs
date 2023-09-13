3702bdcf149a52352805d3028432e607ee7ea8e7
mthem <mthem@INDLEYETHU>
2023-09-13 22:56:15 +0200

    Add project files.

---
+++ Hello world/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Hello Sisekelo");
+                     Console.WriteLine("THis my first c# code");
+          Console.ReadLine();
+
+        }
+    }
+}
